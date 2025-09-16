a13c697ca8c4523b1b9ae1f56676d7db2da14c30
Marcos Ferreira Shirafuchi <marcosfshirafuchi@gmail.com>
2025-09-15 21:49:06 -0300

    Configuring Eureka server

--- hr-eureka-server/src/main/java/com/marcosfshirafuchi/hreurekaserver/HrEurekaServerApplication.java
+++ hr-eureka-server/src/main/java/com/marcosfshirafuchi/hreurekaserver/HrEurekaServerApplication.java
@@ -2,7 +2,9 @@ package com.marcosfshirafuchi.hreurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class HrEurekaServerApplication {
 
