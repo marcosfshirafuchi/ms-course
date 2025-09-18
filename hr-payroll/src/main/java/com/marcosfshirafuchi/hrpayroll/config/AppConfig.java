d73514b5f73d672386ebd6d9736b979c0d4ea4f8
Marcos Ferreira Shirafuchi <marcosfshirafuchi@gmail.com>
2025-09-17 22:42:41 -0300

    Configuring Eureka clients

--- hr-payroll/src/main/java/com/marcosfshirafuchi/hrpayroll/config/AppConfig.java
+++ hr-payroll/src/main/java/com/marcosfshirafuchi/hrpayroll/config/AppConfig.java
@@ -1,31 +1,13 @@
 package com.marcosfshirafuchi.hrpayroll.config;
-import org.springframework.cloud.loadbalancer.core.ServiceInstanceListSupplier;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.DefaultServiceInstance;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import reactor.core.publisher.Flux;
-
-import java.util.List;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class AppConfig {
-
     @Bean
-    ServiceInstanceListSupplier hrWorkerInstanceSupplier() {
-        return new ServiceInstanceListSupplier() {
-            @Override
-            public String getServiceId() {
-                return "hr-worker";
-            }
-
-            @Override
-            public Flux<List<ServiceInstance>> get() {
-                return Flux.just(List.of(
-                        new DefaultServiceInstance("hr-worker-1", "hr-worker", "localhost", 8001, false),
-                        new DefaultServiceInstance("hr-worker-2", "hr-worker", "localhost", 8002, false)
-                ));
-            }
-        };
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
     }
 }
