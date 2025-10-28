1fdcc3d87792c7c759e7410c05b3a87fd8ca8930
Marcos Ferreira Shirafuchi <marcosfshirafuchi@gmail.com>
2025-10-28 17:19:28 -0300

    UserFeignClient

---
+++ hr-oauth/src/main/java/com/marcosfshirafuchi/hroauth/services/UserService.java
@@ -0,0 +1,29 @@
+package com.marcosfshirafuchi.hroauth.services;
+
+import com.marcosfshirafuchi.hroauth.entities.User;
+import com.marcosfshirafuchi.hroauth.feignclients.UserFeignClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    //Log de onde está saindo da classe
+    private static Logger logger = LoggerFactory.getLogger(UserService.class);
+
+    @Autowired
+    private UserFeignClient userFeignClient;
+
+    public User findByEmail(String email) {
+        //getBody porque a resposta é do ResponseEntity
+        User user = userFeignClient.findByEmail(email).getBody();
+        if (user == null) {
+            logger.error("Email not found: " + email);
+            throw new IllegalArgumentException("Email not found");
+        }
+        logger.info("Email found: " + email);
+        return user;
+    }
+
+}
